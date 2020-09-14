42bbb53bea226233cf11b9acb997bc06d1f49a75
lemon-piepie <purplechilde@163.com>
2020-09-14 19:00:01 +0800

    finish 13

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,12 @@
+import axios from "axios"
+
+jest.mock("axios")
+
+const { default: getUsers } = require("../users");
+
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
+  test("should get users data with mock axios get", async() => {
     // TODO 13: add async test with manual mock
+    await expect(getUsers()).resolves.toEqual({})
   });
 });
